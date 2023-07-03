7346ed36ef13eac4e961b9e9997a2d3e9d230bd2
JSanLazaro <jsanlazaro@hotmail.com>
2023-07-03 21:36:27 +0200

    refactored index method HomeController

--- src/main/java/org/factoriaf5/zootopia/controllers/HomeController.java
+++ src/main/java/org/factoriaf5/zootopia/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package org.factoriaf5.zootopia.controllers;
 
+import org.factoriaf5.zootopia.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping(path = "/users")
-    public String index(){
-        return "Hello";
+    public User index(){
+        User jesus = new User("Jesus","12345678");
+        return jesus;
     }
 }
