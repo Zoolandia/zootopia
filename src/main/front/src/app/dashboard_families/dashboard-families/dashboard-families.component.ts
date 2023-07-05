7d51b54c489ee2dbe5de037f0c8b781f7e7ac4fe
PabloEsteban87 <pablosebas87@gmail.com>
2023-07-05 16:24:21 +0200

    Inicio angular 2

---
+++ src/main/front/src/app/dashboard_families/dashboard-families/dashboard-families.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-families',
+  templateUrl: './dashboard-families.component.html',
+  styleUrls: ['./dashboard-families.component.scss']
+})
+export class DashboardFamiliesComponent {
+
+}
