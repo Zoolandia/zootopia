ca3e0c16995256716908b1006eee598c3d424747
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-06 12:30:00 +0200

    Edit app components to ok

--- src/main/front/src/app/user/views/login/login.component.ts
+++ src/main/front/src/app/user/views/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { AccessformComponent } from '../../components/accessform/accessform.component';
 
 @Component({
+   
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
