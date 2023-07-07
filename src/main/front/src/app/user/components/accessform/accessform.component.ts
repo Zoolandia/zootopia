26aa0ac2c37fc0e7f25553fe37af782d82a75eed
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-07 15:36:22 +0200

    create error log

--- src/main/front/src/app/user/components/accessform/accessform.component.ts
+++ src/main/front/src/app/user/components/accessform/accessform.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './accessform.component.html',
   styleUrls: ['./accessform.component.scss']
 })
-export class AccessformComponent {
+export class AccessFormComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  login() {
+
+    if (this.username === 'admin' && this.password === 'admin') {
+      // Inicio de sesión exitoso
+      console.log('Inicio de sesión exitoso');
+    } else {
+      // Generar error y mostrar mensaje
+      this.errorMessage = 'Los datos introducidos no son correctos, por favor compruébelos.';
+    }
+  }
 }
