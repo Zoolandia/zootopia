2daa2511ff874533052181ba347301088cf8e48c
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-07 15:36:47 +0200

    create error log

--- src/main/front/src/app/app.module.ts
+++ src/main/front/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './user/views/login/login.component';
-import { AccessformComponent } from './user/components/accessform/accessform.component';
+import { AccessFormComponent } from './user/components/accessform/accessform.component';
 import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
@@ -12,9 +12,10 @@ import { HeaderComponent } from './shared/header/header.component';
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    AccessformComponent
+    AccessFormComponent,
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule
   ],
