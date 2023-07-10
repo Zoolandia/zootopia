59fd752308113f5de3bd734480064f9b622a1a59
DirtdiverIV <davidgomez.seg@gmail.com>
2023-07-10 13:42:28 +0200

    Created Springboot Packages, Classes, Entitites and Getter and Setters

---
+++ src/main/java/org/factoriaf5/zootopia/models/users/Users.java
@@ -0,0 +1,45 @@
+package org.factoriaf5.zootopia.models.users;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
+public class Users {
+    public Users(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
