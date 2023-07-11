993922f45b5d8b4aaf00c89ebdfe699bb328693b
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-07-11 13:03:19 +0100

    mocked edit functionality test

--- src/main/front/src/app/app-routing.module.ts
+++ src/main/front/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './user/views/login/login.component';
 import { HeaderComponent } from './shared/header/header.component';
-
+import { EditFormComponent } from './user/components/edit-form/edit-form.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent }, 
   { path: 'header', component: HeaderComponent }, 
-  
+  { path: 'header', component: HeaderComponent, children: [
+    { path: 'edit/:id', component: EditFormComponent }
+  ]}
 ];
 
 
--- src/main/front/src/app/user/components/edit-form/edit-form.component.ts
+++ src/main/front/src/app/user/components/edit-form/edit-form.component.ts
@@ -1,18 +1,78 @@
 import { Component } from '@angular/core';
-
+import { Animal } from '../../../models/Animal.model';
+import { animals } from 'src/app/mockup-animals';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-edit-form',
   templateUrl: './edit-form.component.html',
   styleUrls: ['./edit-form.component.scss']
 })
 export class EditFormComponent {
-  name: string ="";
-  type: string ="";
-  family: string ="";
-  gender: string ="";
-  admission: string ="";
+  animal: Animal | undefined;
+  animalId: number;
+  name: string = "";
+  type: string = "";
+  family: string = "";
+  gender: string = "";
+  admission: string = "";
   image: File | null = null;
 
+  constructor(private route: ActivatedRoute) {
+    
+    this.animalId = 0; 
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const animalIdParam = params.get('id');
+      if (animalIdParam !== null) {
+        this.animalId = +animalIdParam;
+        this.animal = this.getAnimalById(this.animalId);
+        this.populateFormWithAnimalData();
+      }
+    });
+  }
+
+  getAnimalById(id: number): Animal | undefined {
+    
+    return animals.find(animal => animal.id === id);
+  }
+
+  populateFormWithAnimalData() {
+    if (this.animal) {
+      this.name = this.animal.name;
+      this.type = this.animal.type;
+      this.family = this.animal.family;
+      this.gender = this.animal.gender;
+      this.admission = this.formatDate(this.animal.admission);
+      if (this.animal.image) {
+        this.convertImageUrlToFile(this.animal.image)
+          .then(file => {
+            this.image = file;
+          })
+          .catch(error => {
+            console.error('Error converting image URL to file:', error);
+          });
+      }
+      console.log(this.animal)
+    }
+  }
+  formatDate(date: Date): string {
+    const formattedDate = new Date(date).toISOString().split('T')[0];
+    return formattedDate;
+  }
+  convertImageUrlToFile(imageUrl: string): Promise<File> {
+    return fetch(imageUrl)
+      .then(response => response.blob())
+      .then(blob => {
+        const file = new File([blob], 'animalImage', { type: blob.type });
+        return Promise.resolve(file);
+      })
+      .catch(error => {
+        console.error('Error fetching image:', error);
+        return Promise.reject(error);
+      });
+  }
   onFileSelected(event: any) {
     this.image = event.target.files[0];
   }
@@ -20,19 +80,27 @@ export class EditFormComponent {
     return URL.createObjectURL(file);
   }
   submitForm() {
-
-    const formData = {
+    const updatedAnimal: Animal = {
+      id: this.animalId,
       name: this.name,
       type: this.type,
       family: this.family,
       gender: this.gender,
-      admission: this.admission,
-      image: this.image
+      admission: new Date(this.admission), 
+      image: this.image ? URL.createObjectURL(this.image) : '',
+      origin: this.animal?.origin ?? ""
     };
-
-    
-    console.log(formData);
-   
+  
+    console.log(updatedAnimal);
+  
+    const index = animals.findIndex(animal => animal.id === this.animalId);
+    if (index !== -1) {
+      animals[index] = updatedAnimal;
+      console.log('Datos actualizados:', animals);
+      
+    } else {
+      console.log('Animal no encontrado en el JSON.');
+    }
   }
 }
 
