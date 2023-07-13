c7656102ab94091bb32d2eec7995a2088091ebb2
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-13 14:37:12 +0200

    login detect user and password for BBDD

--- src/main/front/src/app/user/components/accessform/accessform.component.ts
+++ src/main/front/src/app/user/components/accessform/accessform.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
+interface User {
+  id: number;
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-accessform',
@@ -10,12 +17,17 @@ export class AccessFormComponent {
   username: string = '';
   password: string = '';
   errorMessage: string = '';
-  constructor(private router: Router) {}
+
+  constructor(private router: Router, private http: HttpClient) {}
+
   login() {
-    if (this.username === 'admin' && this.password === 'admin') {
-      this.router.navigate(['/header']);
-    } else {
-      this.errorMessage = 'Los datos introducidos no son correctos, por favor compruébelos.';
-    }
+    this.http.get<User[]>('http://localhost:8000/users').subscribe((usuarios: User[]) => {
+      const foundUser = usuarios.find(u => u.username === this.username && u.password === this.password);
+      if (foundUser) {
+        this.router.navigate(['/header']);
+      } else {
+        this.errorMessage = 'Los datos introducidos no son correctos, por favor compruébelos.';
+      }
+    });
   }
 }
